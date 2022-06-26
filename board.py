e25be5bbb04e6951daab1cf42948408ad2a66b69
Simone Morra <simorra95@gmail.com>
2022-06-26 11:09:49 +0200

    Refactor check for tris

--- board.py
+++ board.py
@@ -11,6 +11,54 @@ class Board:
     separator = '\n' + '+'.join(['-']*self.dim) + '\n'
     return separator.join(rows)
 
+  def has_horizontal_tris(self, player):
+    if player not in ['X', 'O']:
+      raise ValueError(f'Player should be one of "X" or "O", received {player}')
+    
+    for i in range(self.dim):
+      for j in range(self.dim):
+        if self.cells[i*self.dim + j] != player:
+          break
+      else:
+        return True
+    return False
+
+  def has_vertical_tris(self, player):
+    if player not in ['X', 'O']:
+      raise ValueError(f'Player should be one of "X" or "O", received {player}')
+
+    for j in range(self.dim):
+      for i in range(self.dim):
+        if self.cells[i*self.dim + j] != player:
+          break
+      else:
+        return True
+    return False
+
+  def has_diagonal_tris(self, player):
+    if player not in ['X', 'O']:
+      raise ValueError(f'Player should be one of "X" or "O", received {player}')
+
+    # Main diagonal
+    for i in range(self.dim):
+      if self.cells[i*self.dim + i] != player:
+        break
+    else:
+      return True
+
+    # Secondary diagonal
+    for i in range(self.dim):
+      if self.cells[i*self.dim + self.dim-1-i] != player:
+        break
+    else:
+      return True
+
+    return False
+
+  def has_tris(self, player):
+    return self.has_horizontal_tris(player) or self.has_vertical_tris(player) or \
+           self.has_diagonal_tris(player)
+
   def is_in_terminal_state(self):
     # Check if the board is full
     for cell in self.cells:
@@ -19,51 +67,12 @@ class Board:
     else:
       return True
 
-    # Check for horizontal "tris"
-    for i in range(self.dim):
-      if self.cells[i*self.dim] == ' ':
-        continue
-      for j in range(1, self.dim):
-        # All the cells in the row must contain the same value as the first
-        if self.cells[i*self.dim + j] != self.cells[i*self.dim]:
-          break
-      else:
-        return True
-
-    # Check for vertical "tris"
-    for j in range(self.dim):
-      if self.cells[j] == ' ':
-        continue
-      for i in range(1, self.dim):
-        # All the cells in the column must contain the same value as the first
-        if self.cells[i*self.dim + j] != self.cells[j]:
-          break
-      else:
-        return True
-
-    # Check for diagonal "tris"
-    if self.cells[0] != ' ':
-      for i in range(1, self.dim):
-        if self.cells[i*self.dim + i] != self.cells[0]:
-          break
-      else:
-        return True
-
-    if self.cells[self.dim-1] != ' ':
-      for i in range(1, self.dim):
-        if self.cells[i*self.dim + self.dim-1-i] != self.cells[self.dim-1]:
-          break
-      else:
-        return True
-
-    return False
+    return self.has_tris('X') or self.has_tris('O')
 
   def apply_move(self, player, row, col):
     if self.is_in_terminal_state():
       raise RuntimeError("Can't apply new moves to board in terminal state")
-
-    # Check input arguments
-    if player not in 'XO':
+    if player not in ['X', 'O']:
       raise ValueError(f'Player should be one of "X" or "O", received {player}')
     if self.cells[row*self.dim + col] != ' ':
       raise ValueError(f'The cell {row}, {col} is already occupied')
